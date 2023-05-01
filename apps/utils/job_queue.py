057ea3faa32ef79fad748d9f7d410555e8374aa5
张建平 <seven_nighter@163.com>
2023-05-01 18:13:52 +0800

    rabbitmq配置优化

--- apps/utils/job_queue.py
+++ apps/utils/job_queue.py
@@ -11,6 +11,15 @@ class RabbitQueue:
         self.host = host or settings.RABBITMQ_HOST
         self.port = port or settings.RABBITMQ_PORT
         self.channel = None
+        self.push_key = "_device_push"
+
+    @staticmethod
+    def generate_body(topic, data):
+        """生成通用消息体"""
+        return json.dumps({
+            "topic": topic,
+            "data": data
+        })
 
     def connect(self):
         credit = pika.PlainCredentials(username=self.username, password=self.password)
@@ -20,25 +29,35 @@ class RabbitQueue:
             )
         ).channel()
 
-    def push_enqueue(self, image_list):
-        """推送数据"""
+    def push_enqueue(self, topic, data):
+        """
+        推送数据
+        {
+            "topic": "topic",
+            "data": "data",
+        }
+        """
         self.connect()
         channel = self.channel
-        key = "_device_push"
-        channel.queue_declare(queue=key, durable=True)  # 队列持久化
+        channel.queue_declare(queue=self.push_key, durable=True)  # 队列持久化
         channel.basic_publish(
             exchange="",
-            routing_key=key,
-            body=json.dumps(image_list, ensure_ascii=False),
+            routing_key=self.push_key,
+            body=self.generate_body(topic, data),
             properties=pika.BasicProperties(
                 delivery_mode=2,  # 消息持久化
             ),
         )
-
         channel.close()
 
     def pop_queue(self, queue_name, timeout=0):
-        """获取数据"""
+        """
+        获取数据
+        {
+            "topic": "topic",
+            "data": "data",
+        }
+        """
         self.connect()
         channel = self.channel
 
@@ -54,3 +73,8 @@ class RabbitQueue:
         """数据回调"""
         receive = json.loads(body.decode())
         print(receive)
+        method_name = receive.get("topic")
+        if not callable(method_name):
+            print("未知主题，不可调用")
+        else:
+            method_name(receive.get("data"))
